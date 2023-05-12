ffc7ad234761a462e7e6f881663ee401926e8755
duyou <2582355704@qq.com>
2023-05-12 18:08:31 +0800

    Add project files.

---
+++ SkillSystem/Server/Program.cs
@@ -0,0 +1,52 @@
+global using SkillSystem.Shared;
+global using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.ResponseCompression;
+using SkillSystem.Server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//×¢²áDbContext
+builder.Services.AddDbContext<DataContext>(option =>
+{
+	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+//Ìí¼ÓswaggerUI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//Ìí¼ÓswaggerUI
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseWebAssemblyDebugging();
+}
+else
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSwagger();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
